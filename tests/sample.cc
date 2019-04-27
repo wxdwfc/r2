f434c4b9fa916000b6d5e1214be9dba59b2516d3
wxd <wxdwfc@gmail.com>
2019-04-27 13:18:10 +0800

    test done

--- tests/sample.cc
+++ tests/sample.cc
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
 
+#include "test_ud.hpp"
+
 TEST(FactorialTest, MagicNumber) {
   EXPECT_EQ(72 + 1, 73);
+}
+
+TEST(FactorialTest_1, MagicNumber) {
+  EXPECT_EQ(72 + 1, 72);
+}
+
+TEST(FactorialTest_1, MagicNumber3) {
+  EXPECT_EQ(72 + 1, 72);
 }
 
 int main(int argc, char **argv) {
