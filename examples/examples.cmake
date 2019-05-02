c00b999a07bb8d9fc99da499533d71ed0e4caa12
wxd <wxdwfc@gmail.com>
2019-05-02 19:19:18 +0800

    refine examples

--- examples/examples.cmake
+++ examples/examples.cmake
@@ -2,5 +2,10 @@ file(GLOB R_SOURCES "src/*.cc")
 
 include_directories("src")
 include_directories("deps/boost/include")
+
+## routine example
 add_executable(routine examples/routine_example.cc ${R_SOURCES})
 target_link_libraries(routine boost_coroutine boost_chrono boost_thread boost_context boost_system)
+
+add_executable(timer examples/timer_cycle.cc)
+
