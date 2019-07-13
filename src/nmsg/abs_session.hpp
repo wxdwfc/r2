0a12a2d6e2efab4ae11b1196095b37e4349096c8
wxd <wxdwfc@gmail.com>
2019-07-13 20:46:13 +0800

    template code for messaging

--- src/nmsg/abs_session.hpp
+++ src/nmsg/abs_session.hpp
@@ -10,21 +10,42 @@
 namespace r2
 {
 /*!
-    Session manages a connection from myself to a specific remote point
+    Session manages a connection from myself to a specific remote point.
 */
 #define R2Future class
 template <R2Future Future>
 class Session
 {
 public:
+    constexpr double no_timeout = std::numeric_limits<double>::max();
     /*!
         Send a message, and spawn a future in the scheduler
+
+        \param msg: the sending msg
+        \param timeout: time out recorded in **ms**
+
         usage example:
-            auto future = session.send(msg);        
+            auto future = session.send(msg,120);        
             R2_SPAWN(future);
             R2_YIELD;
      */
-    Future send(Msg *msg) = 0;
+    virtual Future
+    send(Msg *msg,
+         const double timeout = no_timeout) = 0;
+
+    /*!
+        A blocking version of send.
+
+        \param msg: the sending msg
+        \param timeout: time out recorded in **ms**
+
+        usage example:
+            auto ret = session.send_blocking(msg,120); // 120 ms
+            ASSERT(ret == SUCC);
+     */
+    virtual rdmaio::IOStatus
+    send_blocking(Msg *msg,
+                  const double timeout = no_timeout) = 0;
 };
 
 } // namespace r2
