545404e1877d758e5605b5c671a1d83c4507ee7b
wxd <wxdwfc@gmail.com>
2019-07-13 17:40:37 +0800

    Add msg class

--- src/nmsg/abs_session.hpp
+++ src/nmsg/abs_session.hpp
@@ -4,4 +4,14 @@
 
 namespace r2
 {
-}
+/*!
+    Session manages a connection from myself to a specific remote point
+*/
+#define R2Future class
+template <R2Future Future>
+class Session
+{
+public:
+};
+
+} // namespace r2
