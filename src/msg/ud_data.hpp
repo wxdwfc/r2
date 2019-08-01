603a12148ab1f525d03570b737e4b6fe7e602e53
wxd <wxdwfc@gmail.com>
2019-08-01 11:00:48 +0800

    minor

--- src/msg/ud_data.hpp
+++ src/msg/ud_data.hpp
@@ -10,15 +10,18 @@ const int MAX_UD_RECV_SIZE = 2048;
 const int MAX_UD_PACKET_SIZE = 4096;
 const int GRH_SIZE = 40;
 
-struct UdConnectInfo {
-  ibv_ah *address_handler = nullptr;
+struct UdConnectInfo
+{
+  ibv_ah* address_handler = nullptr;
   u64 remote_qpn = 0;
   u64 remote_qkey = 0;
 };
 
-struct UdSender {
+struct UdSender
+{
 
-  UdSender(const Addr &addr, uint64_t lkey) {
+  UdSender(const Addr& addr, uint64_t lkey)
+  {
 
     for (uint i = 0; i < MAX_UD_SEND_DOORBELL; ++i) {
       wrs_[i].opcode = IBV_WR_SEND_WITH_IMM;
@@ -31,10 +34,11 @@ struct UdSender {
     }
   }
 
-  inline rdmaio::IOStatus flush_pending(rdmaio::UDQP *qp) {
+  inline rdmaio::IOStatus flush_pending(rdmaio::UDQP* qp)
+  {
     if (current_window_idx_ > 0) {
       wrs_[current_window_idx_ - 1].next = nullptr;
-      struct ibv_send_wr *bad_sr_ = nullptr;
+      struct ibv_send_wr* bad_sr_ = nullptr;
       auto ret = ibv_post_send(qp->qp_, &wrs_[0], &bad_sr_);
       ASSERT(ret == 0) << "; w error: " << strerror(errno);
       wrs_[current_window_idx_ - 1].next = &wrs_[current_window_idx_];
@@ -43,8 +47,8 @@ struct UdSender {
     return rdmaio::SUCC;
   }
 
-  ibv_send_wr &cur_wr() { return wrs_[current_window_idx_]; }
-  ibv_sge &cur_sge() { return sges_[current_window_idx_]; }
+  ibv_send_wr& cur_wr() { return wrs_[current_window_idx_]; }
+  ibv_sge& cur_sge() { return sges_[current_window_idx_]; }
 
   ibv_send_wr wrs_[MAX_UD_SEND_DOORBELL];
   ibv_sge sges_[MAX_UD_SEND_DOORBELL];
@@ -52,9 +56,11 @@ struct UdSender {
   uint current_window_idx_ = 0;
 };
 
-struct UdReceiver {
+struct UdReceiver
+{
 
-  UdReceiver(rdmaio::UDQP *qp, int max_msg_size = MAX_UD_PACKET_SIZE) {
+  UdReceiver(rdmaio::UDQP* qp, int max_msg_size = MAX_UD_PACKET_SIZE)
+  {
 
     ASSERT(qp != nullptr);
     auto allocator = AllocatorMaster<>::get_thread_allocator();
@@ -65,7 +71,8 @@ struct UdReceiver {
 
     // fill in the default values of recv wrs/sges
     for (uint i = 0; i < MAX_UD_RECV_SIZE; ++i) {
-      struct ibv_sge sge {
+      struct ibv_sge sge
+      {
         .addr = (uintptr_t)allocator->alloc(max_msg_size),
         .length = (uint32_t)max_msg_size, .lkey = qp->local_mem_.key
       };
@@ -79,7 +86,7 @@ struct UdReceiver {
       rrs_[i].next = (i < (MAX_UD_RECV_SIZE - 1)) ? &rrs_[i + 1] : &rrs_[0];
     }
 #if R2_SOLICITED
-    if (ibv_req_notify_cq(qp->recv_cq_, 1)) {
+    if (qp->event_channel && ibv_req_notify_cq(qp->recv_cq_, 1)) {
       ASSERT(false) << "Couldn't request CQ notification";
     }
 #endif
@@ -89,12 +96,13 @@ struct UdReceiver {
   struct ibv_recv_wr rrs_[MAX_UD_RECV_SIZE];
   struct ibv_sge sges_[MAX_UD_RECV_SIZE];
   struct ibv_wc wcs_[MAX_UD_RECV_SIZE];
-  struct ibv_recv_wr *bad_rr_;
+  struct ibv_recv_wr* bad_rr_;
 
   // current header which pointes to the rrs
   int recv_head_ = 0;
 
-  rdmaio::IOStatus post_recvs(rdmaio::UDQP *qp, int num) {
+  rdmaio::IOStatus post_recvs(rdmaio::UDQP* qp, int num)
+  {
     if (unlikely(num <= 0))
       return rdmaio::SUCC;
 
--- src/msg/ud_msg.cc
+++ src/msg/ud_msg.cc
@@ -9,14 +9,20 @@ namespace r2 {
 
 const int max_idle_num = 1;
 
-UdAdapter::UdAdapter(const Addr &my_addr, UDQP *sqp, UDQP *qp)
-    : my_addr(my_addr), send_qp_(sqp), qp_(qp ? qp : sqp),
-      sender_(my_addr, qp_->local_mem_.key), receiver_(qp_) {}
+UdAdapter::UdAdapter(const Addr& my_addr, UDQP* sqp, UDQP* qp)
+  : my_addr(my_addr)
+  , send_qp_(sqp)
+  , qp_(qp ? qp : sqp)
+  , sender_(my_addr, qp_->local_mem_.key)
+  , receiver_(qp_)
+{}
 
-static ibv_ah *create_ah(UDQP *qp, const QPAttr &attr);
+static ibv_ah*
+create_ah(UDQP* qp, const QPAttr& attr);
 
-IOStatus UdAdapter::connect(const Addr &addr, const rdmaio::MacID &id,
-                            int uid) {
+IOStatus
+UdAdapter::connect(const Addr& addr, const rdmaio::MacID& id, int uid)
+{
 
   // check if we already connected
   if (connect_infos_.find(addr.to_u32()) != connect_infos_.end())
@@ -28,25 +34,27 @@ IOStatus UdAdapter::connect(const Addr &addr, const rdmaio::MacID &id,
     auto ah = create_ah(qp_, fetched_attr);
     if (ah == nullptr)
       return ERR;
-    UdConnectInfo connect_info = {.address_handler = ah,
-                                  .remote_qpn = fetched_attr.qpn,
-                                  .remote_qkey = fetched_attr.qkey};
+    UdConnectInfo connect_info = { .address_handler = ah,
+                                   .remote_qpn = fetched_attr.qpn,
+                                   .remote_qkey = fetched_attr.qkey };
     connect_infos_.insert(std::make_pair(addr.to_u32(), connect_info));
     return SUCC;
   } else
     return ret;
 }
 
-IOStatus UdAdapter::send_async(const Addr &addr, const char *msg, int size) {
+IOStatus
+UdAdapter::send_async(const Addr& addr, const char* msg, int size)
+{
 
-  auto &wr = sender_.cur_wr();
-  auto &sge = sender_.cur_sge();
+  auto& wr = sender_.cur_wr();
+  auto& sge = sender_.cur_sge();
 
-  const auto &it = connect_infos_.find(addr.to_u32());
+  const auto& it = connect_infos_.find(addr.to_u32());
   if (unlikely(it == connect_infos_.end()))
     return NOT_CONNECT;
 
-  const auto &link_info = it->second;
+  const auto& link_info = it->second;
 
   wr.wr.ud.ah = link_info.address_handler;
   wr.wr.ud.remote_qpn = link_info.remote_qpn;
@@ -55,7 +63,8 @@ IOStatus UdAdapter::send_async(const Addr &addr, const char *msg, int size) {
   wr.send_flags = (send_qp_->empty() ? IBV_SEND_SIGNALED : 0) |
                   ((size < ::rdmaio::MAX_INLINE_SIZE) ? IBV_SEND_INLINE : 0);
 #if R2_SOLICITED
-  wr.send_flags |= IBV_SEND_SOLICITED;
+  if (qp_->event_channel)
+    wr.send_flags |= IBV_SEND_SOLICITED;
 #endif
 
   if (send_qp_->need_poll()) {
@@ -77,32 +86,39 @@ IOStatus UdAdapter::send_async(const Addr &addr, const char *msg, int size) {
   return SUCC;
 }
 
-IOStatus UdAdapter::flush_pending() { return sender_.flush_pending(send_qp_); }
+IOStatus
+UdAdapter::flush_pending()
+{
+  return sender_.flush_pending(send_qp_);
+}
 
-int UdAdapter::poll_all(const MsgProtocol::msg_callback_t &f) {
+int
+UdAdapter::poll_all(const MsgProtocol::msg_callback_t& f)
+{
 #if R2_SOLICITED
-  struct ibv_cq *ev_cq;
-  void *ev_ctx;
+  struct ibv_cq* ev_cq;
+  if (qp_->event_channel) {
+    void* ev_ctx;
 
-  // waiting for in-coming requests
-  // XD: fixme: this is a blocking call
-  if (ibv_get_cq_event(qp_->event_channel, &ev_cq, &ev_ctx)) {
-    ASSERT(false) << "faild to get the cq event";
-  }
+    // waiting for in-coming requests
+    // XD: fixme: this is a blocking call
+    if (ibv_get_cq_event(qp_->event_channel, &ev_cq, &ev_ctx)) {
+      ASSERT(false) << "faild to get the cq event";
+    }
 
-  ibv_ack_cq_events(ev_cq, 1);
-
-  if (ibv_req_notify_cq(ev_cq, 1)) {
-    ASSERT(false) << "Couldn't request CQ notification";
+    if (ibv_req_notify_cq(ev_cq, 1)) {
+      ASSERT(false) << "Couldn't request CQ notification";
+    }
   }
 #endif
   uint poll_result =
-      ibv_poll_cq(qp_->recv_cq_, MAX_UD_RECV_SIZE, receiver_.wcs_);
+    ibv_poll_cq(qp_->recv_cq_, MAX_UD_RECV_SIZE, receiver_.wcs_);
   for (uint i = 0; i < poll_result; ++i) {
     if (likely(receiver_.wcs_[i].status == IBV_WC_SUCCESS)) {
       Addr addr;
       addr.from_u32(receiver_.wcs_[i].imm_data);
-      f((const char *)(receiver_.wcs_[i].wr_id + GRH_SIZE), MAX_UD_PACKET_SIZE,
+      f((const char*)(receiver_.wcs_[i].wr_id + GRH_SIZE),
+        MAX_UD_PACKET_SIZE,
         addr);
     } else {
       ASSERT(false) << "error wc status " << receiver_.wcs_[i].status;
@@ -114,16 +130,24 @@ int UdAdapter::poll_all(const MsgProtocol::msg_callback_t &f) {
     ASSERT(receiver_.post_recvs(qp_, current_idle_recvs_) == SUCC);
     current_idle_recvs_ = 0;
   }
+#if R2_SOLICITED
+  if (qp_->event_channel) {
+    ibv_ack_cq_events(qp_->recv_cq_, 1);
+  }
+#endif
   return poll_result;
 }
 
-Buf_t UdAdapter::get_my_conninfo() {
+Buf_t
+UdAdapter::get_my_conninfo()
+{
   QPAttr res = qp_->get_attr();
   return Marshal::serialize_to_buf(res);
 }
 
-IOStatus UdAdapter::connect_from_incoming(const Addr &addr,
-                                          const Buf_t &connect_info) {
+IOStatus
+UdAdapter::connect_from_incoming(const Addr& addr, const Buf_t& connect_info)
+{
   QPAttr attr;
   if (!Marshal::deserialize(connect_info, attr))
     return ERR;
@@ -131,15 +155,18 @@ IOStatus UdAdapter::connect_from_incoming(const Addr &addr,
   if (ah == nullptr)
     return ERR;
 
-  UdConnectInfo info = {
-      .address_handler = ah, .remote_qpn = attr.qpn, .remote_qkey = attr.qkey};
+  UdConnectInfo info = { .address_handler = ah,
+                         .remote_qpn = attr.qpn,
+                         .remote_qkey = attr.qkey };
   if (connect_infos_.find(addr.to_u32()) != connect_infos_.end())
     connect_infos_.erase(connect_infos_.find(addr.to_u32()));
   connect_infos_.insert(std::make_pair(addr.to_u32(), info));
   return SUCC;
 }
 
-void UdAdapter::disconnect(const Addr &addr) {
+void
+UdAdapter::disconnect(const Addr& addr)
+{
 #if 1
   if (connect_infos_.find(addr.to_u32()) != connect_infos_.end()) {
     auto ah = connect_infos_[addr.to_u32()].address_handler;
@@ -149,9 +176,15 @@ void UdAdapter::disconnect(const Addr &addr) {
 #endif
 }
 
-Iter_p_t UdAdapter::get_iter() { return Iter_p_t(new UDIncomingIter(this)); }
+Iter_p_t
+UdAdapter::get_iter()
+{
+  return Iter_p_t(new UDIncomingIter(this));
+}
 
-static ibv_ah *create_ah(UDQP *qp, const QPAttr &attr) {
+static ibv_ah*
+create_ah(UDQP* qp, const QPAttr& attr)
+{
   struct ibv_ah_attr ah_attr;
   ah_attr.is_global = 1;
   ah_attr.dlid = attr.lid;
