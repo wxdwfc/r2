55eb8d1ca77a4cbeb67290cf472782d33bbc6079
wxd <wxdwfc@gmail.com>
2019-08-01 09:20:42 +0800

    add rdma handlers

---
+++ src/rdma/rdma_handlers.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace r2
+{
+
+namespace rdma
+{
+/*!
+    RDMAHandlers register callback to RdmaCtrl, so that:
+    it handles QP creation requests;
+    it handles QP deletion requests, etc.
+ */
+class RDMAHandlers
+{
+};
+} // namespace rdma
+
+} // namespace r2
