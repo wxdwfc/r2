a4c0a7458f5498f7b574346a9962bedd43531545
wxd <wxdwfc@gmail.com>
2020-10-13 21:49:01 +0800

    minor optional for g++17

--- src/utils/option.hh
+++ src/utils/option.hh
@@ -1,23 +1,12 @@
 #pragma once
 
-#if __cplusplus > 201402L
-#include <optional>
-#else
-/**
- * Disable warning.
- * Basically optional is very useful.
- */
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-W#warnings"
+#if defined(__GNUC__) && __GNUC__ < 7
 #include <experimental/optional>
-#pragma GCC diagnostic pop
+#define optional experimental::optional
+#else
+#include <optional>
 #endif
 
 namespace r2 {
-#if __cplusplus > 201402L
-template <typename T>
 using Option = std::optional<T>;
-#else
-template <typename T> using Option = ::std::experimental::optional<T>;
-#endif
 } // namespace r2
